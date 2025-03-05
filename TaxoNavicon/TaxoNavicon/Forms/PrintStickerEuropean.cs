41d228e7973d5ce0287d4991e75993d95b0dc1e5
rivixxx <ilya.demidov.2003a@mail.ru>
2025-03-05 16:36:53 +0300

    1.1.6v

--- TaxoNavicon/TaxoNavicon/Forms/PrintStickerEuropean.cs
+++ TaxoNavicon/TaxoNavicon/Forms/PrintStickerEuropean.cs
@@ -1,22 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Printing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
-using TaxoNaviconRussian;
+using Font = System.Drawing.Font;
+using Word = Microsoft.Office.Interop.Word;
+using System.Runtime.InteropServices;
 
 namespace TaxoNavicon.Forms
 {
+    
     public partial class PrintStickerEuropean : Form
     {
+        private string filePath;
         private PrintDocument printDocument;
         private PoleDataEuropean poleDataEuropean;
+
+        private Word.Application wordApp;
+        private Word.Document wordDoc;
         public PrintStickerEuropean(PoleDataEuropean poleDataEuropean)
         {
             InitializeComponent();
@@ -44,8 +47,8 @@ namespace TaxoNavicon.Forms
 
             // Установим начальные координаты
             int xOffset = 10; // Начальное смещение по X
-            int yOffset = 10; // Начальное смещение по Y
-            int lineSpacing = 20; // расстояние между строками
+            int yOffset = 5; // Начальное смещение по Y
+            int lineSpacing = 15; // расстояние между строками
 
             // Отображение данных
             g.DrawString("Date: " + poleDataEuropean.dataJob, boldFont, Brushes.Black, xOffset, yOffset);
@@ -59,8 +62,8 @@ namespace TaxoNavicon.Forms
             yOffset += lineSpacing;
             g.DrawString("s/n Tacho: " + poleDataEuropean.serialNumberTahograph, boldFont, Brushes.Black, xOffset, yOffset);
 
-            xOffset = 150;
-            yOffset = 10;
+            xOffset = 160;
+            yOffset = 5;
 
             g.DrawString("VIN: " + poleDataEuropean.vinVehicle, boldFont, Brushes.Black, xOffset, yOffset);
 
@@ -73,26 +76,26 @@ namespace TaxoNavicon.Forms
             yOffset += lineSpacing;
             g.DrawString("k: " + poleDataEuropean.k, boldFont, Brushes.Black, xOffset, yOffset);
 
-            xOffset = 250;
-            yOffset = 10;
+            xOffset = 305;
+            yOffset = 5;
 
             // Возможно, добавьте отступы для визуального разделения
             xOffset += lineSpacing * 2; // Дополнительный отступ перед следующей секцией
 
             // Ниже можно добавить информацию о компании
-            g.DrawString("NaviCon OOO", boldFont, Brushes.Black, xOffset, yOffset);
+            g.DrawString("  NaviCon OOO", boldFont, Brushes.Black, xOffset, yOffset);
             yOffset += lineSpacing;
-            g.DrawString("Bulvar stroiteley st., 3G, \n     Tambov", font, Brushes.Black, xOffset, yOffset);
-            yOffset += lineSpacing + 4;
+            g.DrawString("Bulvar stroiteley st., 3G, \n       Tambov", font, Brushes.Black, xOffset, yOffset);
+            yOffset += lineSpacing + 7;
             g.DrawString("+7(4752)55-94-00", font, Brushes.Black, xOffset, yOffset);
-            yOffset += lineSpacing;
+            yOffset += lineSpacing + 1;
             g.DrawString("navicontmb.ru", font, Brushes.Black, xOffset, yOffset);
 
             // Вертикальный текст "RUS 526"
             string verticalText = "RUS 526";
             // Устанавливаем позицию текста
-            int verticalTextX = 470; // Позиция по X для вертикального текста
-            int verticalTextY = 15;   // Позиция по Y для вертикального текста
+            int verticalTextX = 495; // Позиция по X для вертикального текста
+            int verticalTextY = 12;   // Позиция по Y для вертикального текста
 
             // Поворачиваем графику для вертикального текста
             g.TranslateTransform(verticalTextX, verticalTextY);
@@ -103,13 +106,88 @@ namespace TaxoNavicon.Forms
 
         private void toolStripLabelPrint_Click(object sender, EventArgs e)
         {
-            PrintDialog printDialog = new PrintDialog();
-            printDialog.Document = printDocument;
+            /* string relativePath = @"EuropianSticker.lbx"; // Относительный путь к файлу
+             filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath);
 
+             // Открытие документа Word
+             Word.Application wordApp = new Word.Application();
+             Word.Document doc = null;
+
+             try
+             {
+                 // Открываем файл в Word
+                 doc = wordApp.Documents.Open(filePath);
+
+                 // Заменяем текст (пример замены)
+                 FindAndReplace(doc, "<Data>", poleDataEuropean.dataJob);
+
+                 // Сохраняем и закрываем документ
+                 doc.Save();
+             }
+             catch (Exception ex)
+             {
+                 MessageBox.Show("Ошибка: " + ex.Message);
+             }
+             finally
+             {
+                 // Закрываем документ и приложение Word
+                 if (doc != null)
+                 {
+                     doc.Close();
+                 }
+                 wordApp.Quit();
+             }
+
+
+             // Открытие диалогового окна для выбора принтера
+             PrintDialog printDialog = new PrintDialog();
+             if (printDialog.ShowDialog() == DialogResult.OK)
+             {
+                 // Здесь вы должны указать, как именно печатать файл .lbx
+                 // Например, если у вас есть программа, которая может печатать этот файл:
+                 PrintLbxFile(filePath, printDialog.PrinterSettings.PrinterName);
+             }*/
+
+            // Открытие диалогового окна для выбора принтера
+            PrintDialog printDialog = new PrintDialog();
             if (printDialog.ShowDialog() == DialogResult.OK)
             {
                 printDocument.Print();
             }
+        }
+
+        private void PrintLbxFile(string filePath, string printerName)
+        {
+            // Создаем новый процесс для печати .lbx файла
+            Process process = new Process();
+            process.StartInfo.FileName = "C:\\Program Files (x86)\\Brother\\P-touch Editor\\6\\PtouchEditor6.Wpf.exe"; // Укажите путь к программе для печати
+            process.StartInfo.Arguments = $"{filePath} /P {printerName}"; // Пример аргументов, зависит от программы
+            process.StartInfo.UseShellExecute = false; // Убедитесь, что программа может быть запущена без оболочки
+            process.StartInfo.CreateNoWindow = true; // Не показывать окно программы
+
+            try
+            {
+                process.Start();
+                process.WaitForExit(); // Ожидание завершения процесса
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при отправке на печать: " + ex.Message);
+            }
+        }
+
+
+        private void FindAndReplace(Word.Document doc, string findText, string replaceText)
+        {
+            Word.Find findObject = doc.Application.Selection.Find;
+            findObject.ClearFormatting();
+            findObject.Text = findText;
+            findObject.Replacement.ClearFormatting();
+            findObject.Replacement.Text = replaceText;
+
+            object missing = Type.Missing;
+            findObject.Execute(FindText: missing, ReplaceWith: missing,
+                               Replace: Word.WdReplace.wdReplaceAll);
         }
     }
 }
